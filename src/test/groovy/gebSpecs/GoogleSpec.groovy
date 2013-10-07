7d88421dc2b3867635325051bc893d904f8f373a
seamlessauth <seamlessauth1@yahoo.co.in>
2013-10-07 18:01:34 +0530

    First util class

--- src/test/groovy/gebSpecs/GoogleSpec.groovy
+++ src/test/groovy/gebSpecs/GoogleSpec.groovy
@@ -1,21 +1,27 @@
+package gebSpecs
+
 import spock.lang.*
 import geb.*
 import geb.spock.*
+import utils.commonlib
 
 class GoogleSpec extends GebReportingSpec {
+
+   def cl = new commonlib()
 
     def "the first link should be wikipedia"() {
         when:
         to GoogleHomePage
 
         and:
-        q = "wikipedia"
+        def searchString = cl.getRandomText()
+        q = searchString
 
         then:
         at GoogleResultsPage
 
         and:
-        firstResultLink.text() == "Wikipedia"
+        firstResultLink.text() == searchString
 
         when:
         firstResultLink.click()
---
+++ src/test/groovy/utils/commonlib.groovy
@@ -0,0 +1,25 @@
+package utils
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: anilk
+ * Date: 7/10/13
+ * Time: 10:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class commonlib {
+
+    public String getRandomText() {
+        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        for(int i = 0; i < 5; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+
+        }
+
+        return sb.toString();
+    }
+}
